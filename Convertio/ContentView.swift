c1efccaf18665ef23e7d92fd6d958fe3e0d07c63
W1W1-M <w1w1_m@icloud.com>
2022-06-01 22:42:21 +0200

    Add UI

--- Convertio/ContentView.swift
+++ Convertio/ContentView.swift
@@ -8,9 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var unitInput: Double?
+    @State private var unitOutput: Double?
+    @State private var selectedInput: String = "hours"
+    @State private var selectedOutput: String = "minutes"
+    let timeUnits: Array<String> = ["seconds", "minutes", "hours", "days"]
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ZStack {
+                Color.mint.ignoresSafeArea()
+                Form {
+                    Section {
+                        HStack {
+                            Spacer()
+                            TextField("Enter time amount", value: $unitInput, format: .number)
+                                .multilineTextAlignment(.trailing)
+                                .fixedSize()
+                            Text(selectedInput)
+                        }
+                        Picker("Input", selection: $selectedInput) {
+                            ForEach(timeUnits, id: \.self) {
+                                Text($0).tag($0)
+                            }
+                        }.pickerStyle(.segmented)
+                    } header: {
+                        Text("🕰 Input").foregroundColor(.white)
+                    }
+                    Section {
+                        HStack {
+                            Spacer()
+                            Text(unitOutput ?? 0.0, format: .number)
+                            Text(selectedOutput)
+                        }
+                        Picker("Output", selection: $selectedOutput) {
+                            ForEach(timeUnits, id: \.self) {
+                                Text($0).tag($0)
+                            }
+                        }.pickerStyle(.segmented)
+                    } header: {
+                        Text("⏰ Output").foregroundColor(.white)
+                    }
+                }.navigationTitle("Convertio")
+                .onAppear(perform: {
+                    UITableView.appearance().backgroundColor = .clear
+            })
+            }
+        }
     }
 }
 
