ceecef0d7df8ec759951af55d69a9d4b575a5e5c
W1W1-M <w1w1_m@icloud.com>
2022-06-01 22:06:34 +0200

    Initial Commit

---
+++ Convertio/ConvertioApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConvertioApp.swift
+//  Convertio
+//
+//  Created by William Mead on 01/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct ConvertioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
