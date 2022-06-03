148ef5d18a264309fc64198acf66032e75fb423d
W1W1-M <w1w1_m@icloud.com>
2022-06-03 10:16:58 +0200

    Add logic

--- Convertio/ContentView.swift
+++ Convertio/ContentView.swift
@@ -9,14 +9,44 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var unitInput: Double?
-    @State private var unitOutput: Double?
-    @State private var selectedInput: String = "hours"
-    @State private var selectedOutput: String = "minutes"
-    let timeUnits: Array<String> = ["seconds", "minutes", "hours", "days"]
+    @State private var selectedInput: timeUnits = .hours
+    @State private var selectedOutput: timeUnits = .minutes
+    private var unitOutput: Double? {
+        guard let unitInput = unitInput else {
+            return nil
+        }
+        var inputSeconds: Double = 0.0
+        switch selectedInput {
+        case .seconds:
+            inputSeconds = unitInput
+        case .minutes:
+            inputSeconds = unitInput*60
+        case .hours:
+            inputSeconds = unitInput*3600
+        case .days:
+            inputSeconds = unitInput*86400
+        }
+        switch selectedOutput {
+        case .seconds:
+            return inputSeconds
+        case .minutes:
+            return inputSeconds/60
+        case .hours:
+            return inputSeconds/3600
+        case .days:
+            return inputSeconds/86400
+        }
+    }
+    enum timeUnits: String, CaseIterable {
+        case seconds = "seconds"
+        case minutes = "minutes"
+        case hours = "hours"
+        case days = "days"
+    }
     var body: some View {
         NavigationView {
             ZStack {
-                Color.mint.ignoresSafeArea()
+                Color.orange.ignoresSafeArea()
                 Form {
                     Section {
                         HStack {
@@ -24,29 +54,29 @@ struct ContentView: View {
                             TextField("Enter time amount", value: $unitInput, format: .number)
                                 .multilineTextAlignment(.trailing)
                                 .fixedSize()
-                            Text(selectedInput)
+                            Text(selectedInput.rawValue)
                         }
                         Picker("Input", selection: $selectedInput) {
-                            ForEach(timeUnits, id: \.self) {
-                                Text($0).tag($0)
+                            ForEach(timeUnits.allCases, id: \.self) {
+                                Text($0.rawValue)
                             }
                         }.pickerStyle(.segmented)
                     } header: {
-                        Text("🕰 Input").foregroundColor(.white)
+                        Text("🕰 Input").foregroundColor(.black)
                     }
                     Section {
                         HStack {
                             Spacer()
                             Text(unitOutput ?? 0.0, format: .number)
-                            Text(selectedOutput)
+                            Text(selectedOutput.rawValue)
                         }
                         Picker("Output", selection: $selectedOutput) {
-                            ForEach(timeUnits, id: \.self) {
-                                Text($0).tag($0)
+                            ForEach(timeUnits.allCases, id: \.self) {
+                                Text($0.rawValue)
                             }
                         }.pickerStyle(.segmented)
                     } header: {
-                        Text("⏰ Output").foregroundColor(.white)
+                        Text("⏰ Output").foregroundColor(.black)
                     }
                 }.navigationTitle("Convertio")
                 .onAppear(perform: {
